0c71b83a9d06ba3f7b11e1b5c584939bb4f01f53
Jemima <contatojemima@gmail.com>
2023-02-14 19:52:15 -0300

    create defaultTheme
    - set typing declaration for defaultTheme
    - install @types/styled-components for typing defaultTheme
    - create default theme file

---
+++ src/styles/defaultTheme.ts
@@ -0,0 +1,23 @@
+
+export const defaultTheme = {
+  'white': '#FFF',
+
+  'gray-100': '#fafafa',
+  'gray-200': '#F3F2F2',
+  'gray-300': '#EDEDED',
+  'gray-400': '#E6E5E5',
+  'gray-500': '#D7D5D5',
+
+  'brown-300': '#8D8686',
+  'brown-500': '#574F4D',
+  'brown-700': '#403937',
+  'brown-900': '#272221',
+
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-700': '#4B2995',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-500': '#DBAC2C',
+  'yellow-700': '#C47F17',
+}
