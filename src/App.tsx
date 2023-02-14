25102e962fe4a4a66fcc2604da2e454cb9fe18ee
Jemima <contatojemima@gmail.com>
2023-02-14 20:36:51 -0300

    include global styles into App( )

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom"
 import { Router } from "./Router"
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/defaultTheme"
+import { GlobalStyle } from "./styles/global"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Router />
       </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
