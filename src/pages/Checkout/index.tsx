153c9f967ef28c5b45509da989b409f30c6ce426
jemluz <contatojemima@gmail.com>
2023-07-29 09:37:11 -0300

    Checkout page html markup 1/2

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,5 +1,58 @@
+import { MainGrid } from "../../styles/global";
+import { CheckoutGrid, CompleteOrderForm, DeliveryAdress, FormLabel, OrderOverview, PaymentOptions } from "./styles";
+
 export function Checkout() {
   return (
-    <div></div>
+    <MainGrid>
+      <CheckoutGrid>
+        <CompleteOrderForm>
+          <DeliveryAdress>
+            <FormLabel>
+              <img src="" alt="" />
+              <div>
+                <h3>Endereço de Entrega</h3>
+                <p>Informe o endereço onde deseja receber seu pedido</p>
+              </div>
+            </FormLabel>
+
+            <div>
+              <input type="number" name="cep" id="" />
+            </div>
+            <div>
+              <input type="text" name="street" id="" />
+            </div>
+            <div>
+              <input type="text" name="number" id="" />
+              <input type="text" name="additional info" id="" />
+            </div>
+            <div>
+              <input type="text" name="neighborhood" id="" />
+              <input type="text" name="city" id="" />
+              <input type="text" name="uf" id="" />
+            </div>
+          </DeliveryAdress>
+
+          <PaymentOptions>
+            <FormLabel>
+              <img src="" alt="" />
+              <div>
+                <h3>Pagamento</h3>
+                <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+              </div>
+            </FormLabel>
+
+            <select name="payment_method" id="">
+              <option value="credit_card">Cartão de crédito</option>
+              <option value="debit_card">cartão de débito</option>
+              <option value="cash">Dinheiro</option>
+            </select>
+          </PaymentOptions>
+        </CompleteOrderForm>
+
+        <OrderOverview>
+
+        </OrderOverview>
+      </CheckoutGrid>
+    </MainGrid>
   )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const CheckoutGrid = styled.div`
+  display: grid;
+  grid-template-columns: 40rem 28rem;
+  gap: 2rem;
+`
+
+export const CompleteOrderForm = styled.div`
+`
+
+export const FormLabel = styled.div`
+
+`
+
+export const DeliveryAdress = styled.section``
+
+export const PaymentOptions = styled.section``
+
+export const OrderOverview = styled.section`
+
+`
